8bd315387b9c77bee7a8053f9be4f17ea07b82bd
BogdanGlazkov <bglazkov@i.ua>
2023-06-29 22:16:37 +0300

    Add responsive layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,12 @@ module.exports = {
       padding: {
         big: "50px",
       },
+      screens: {
+        sm: "480px",
+        md: "768px",
+        lg: "1028px",
+        xl: "1440px",
+      },
     },
   },
   plugins: [],
